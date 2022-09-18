0597e11f22a0552d74e831ea731896c2cf42ed06
Juan Quintero <juanestquintero@gmail.com>
2022-09-18 18:52:13 -0500

    feat(api): showAppName from db

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,21 @@
 import { Component } from '@angular/core';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-
-  title = 'angular-template';
+  public title: string = 'angular-template';
   public appName: string = 'Angular Template';
 
-  showAppName(): void {
-    this.appName = 'Academy';
+  constructor(private appService: AppService) {}
+
+  async showAppName(): Promise<void> {
+    // this.appName = 'Academy';
+    await this.appService.getAppName().subscribe(appName => {
+      this.appName = appName.api;
+    });
   }
 }
