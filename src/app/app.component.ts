267849f099faa43e18b159b2126cb556acb8858b
Juan Quintero <juanestquintero@gmail.com>
2022-09-18 16:46:54 -0500

    feat(app): show static app name method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'angular-template';
+  public appName: string = 'Angular Template';
+
+  showAppName(): void {
+    this.appName = 'Academy';
+  }
 }
