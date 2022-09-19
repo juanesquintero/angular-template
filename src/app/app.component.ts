d397a33256740063eebde96d8ed8e8e86032250c
Juan Quintero <juanestquintero@gmail.com>
2022-09-18 22:09:56 -0500

    chore: app name upper case and replace

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-template';
+  public title: string = 'angular-template';
+  public appName: string = this.title.replace('-', ' ').toUpperCase();
+
+  constructor(private appService: AppService) {}
+
+  showAppName(): void {
+    this.appService.getAppName().subscribe(appName => {
+      this.appName = appName.api.toUpperCase();
+    });
+  }
 }
