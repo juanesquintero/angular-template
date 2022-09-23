d63928b6f3e1f52517d1c8fb342d3d3560f90595
Juan Quintero <juanestquintero@gmail.com>
2022-09-23 09:11:05 -0500

    feat: courses.service and getCourses

--- src/app/courses/list/courses-list.component.ts
+++ src/app/courses/list/courses-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ICourseDTO } from 'src/app/shared/models/courses.model';
+import { CoursesService } from '../courses.service';
 
 @Component({
   selector: 'app-courses-list',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./courses-list.component.scss']
 })
 export class CoursesListComponent implements OnInit {
+  public courses: ICourseDTO[] = []
+  public table: { columns: string[] } = {
+    columns: []
+  }
 
-  constructor() { }
+  constructor(private coursesService: CoursesService) { }
 
   ngOnInit(): void {
+    this.coursesService.getCourses().subscribe((res: ICourseDTO[]) => {
+      this.courses = res;
+      this.table.columns = Object.keys(res[0]) as string[];
+    });
   }
 
 }
