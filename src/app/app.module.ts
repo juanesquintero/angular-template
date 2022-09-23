98baa047e4d4628d4e345d47a1894d1dfdf1b857
Juan Quintero <juanestquintero@gmail.com>
2022-09-22 22:45:02 -0500

    feat: mat table for courses

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
-import { HttpClientModule } from '@angular/common/http';
 import { Component, NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatTableModule } from '@angular/material/table';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
 import { HomeService } from './home/home.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSortModule } from '@angular/material/sort';
 
 @Component({
   selector: 'app-root',
@@ -22,7 +24,9 @@ export class AppComponent {
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSortModule,
+    MatTableModule,
   ],
   providers: [HomeService],
   bootstrap: [AppComponent]
