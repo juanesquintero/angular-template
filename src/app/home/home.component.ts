669a7cff483eb3aaf7e1d6f7e0a2b4798106c463
Juan Quintero <juanestquintero@gmail.com>
2022-10-03 09:48:25 -0500

    refactor: home initial state

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { HomeService } from './home.service';
 import { APP_ROUTES } from '../shared/constants';
 import { IAppName } from '../shared/models';
+
+const initialAppName = 'Angular Template';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -9,16 +11,23 @@ import { IAppName } from '../shared/models';
   encapsulation: ViewEncapsulation.None,
 })
 export class HomeComponent implements OnInit {
-  public appName: string = 'Angular Template';
+  public appName: string = initialAppName;
   public routes = APP_ROUTES;
+
+  public get init() : boolean {
+    return this.appName === initialAppName
+  }
+
 
   constructor(private homeService: HomeService) { }
 
   ngOnInit(): void { }
 
   showAppName(): void {
-    this.homeService.getAppName().subscribe((res: IAppName) => {
-      this.appName = res.api.toUpperCase();
-    });
+    if (this.init) {
+      this.homeService.getAppName().subscribe((res: IAppName) => {
+        this.appName = res.api.toUpperCase();
+      });
+    }
   }
 }
