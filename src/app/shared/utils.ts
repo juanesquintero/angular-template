3f2c90e862d74531759dda0a58e005154402ff32
Juan Quintero <juanestquintero@gmail.com>
2022-10-11 12:44:41 -0500

    chore: hasProperties mixin

--- src/app/shared/utils.ts
+++ src/app/shared/utils.ts
@@ -1,3 +1,12 @@
+// import { keys } from 'ts-transformer-keys';
+
 export const title = (text: string) => {
   return text[0].toUpperCase() + text.slice(1);
 };
+
+// export const hasProperties = (obj: any, Interface: any) => {
+//   const properties = keys<Interface>();
+//   return properties.reduce((acc, next)=>{
+//     return obj.hasOwnProperty(acc) && obj.hasOwnProperty(next);
+//   })
+// }
