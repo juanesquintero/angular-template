5c83902549336a822f70b91df1b7e56e5f22a638
Juan Quintero <juanestquintero@gmail.com>
2022-10-11 12:07:35 -0500

    refactor(courses): actionMsg method

--- src/app/courses/list/courses-list.component.ts
+++ src/app/courses/list/courses-list.component.ts
@@ -1,4 +1,5 @@
 import * as _ from 'lodash';
+import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -6,8 +7,8 @@ import { IAction, IModalMode } from '../../shared/models';
 import { ICourseBase, ICourseDTO, ICourseRow, ICoursesTable } from '../../shared/models/courses.model';
 import { CoursesService } from '../courses.service';
 import { CourseModalComponent } from '../modal/course-modal.component';
-import { Observable } from 'rxjs';
 
+type IModalResult = { course: ICourseDTO, action: IAction };
 @Component({
   selector: 'app-courses-list',
   templateUrl: './courses-list.component.html',
@@ -55,29 +56,41 @@ export class CoursesListComponent implements OnInit {
     this.onCloseModal(dialogRef);
   }
 
-  onCloseModal(dialogRef: MatDialogRef<CourseModalComponent>): void{
-    dialogRef.afterClosed().subscribe((result: {course: ICourseDTO, action: IAction}) => {
+  onCloseModal(dialogRef: MatDialogRef<CourseModalComponent>): void {
+    dialogRef.afterClosed().subscribe((result: IModalResult) => {
       const { course, action } = result;
       if (result) {
         (this[action](course as any) as Observable<any>).subscribe(
           (res: ICourseDTO) => {
-            this._snackBar.open(`${result.action}d ${res.id}`, '', { panelClass: ['snackbar--success'] });
+            this.actionMsg(result, res);
           },
           err => {
-            this._snackBar.open(`${result.action} Error`, '', { panelClass: ['snackbar--error'] });
-            console.error(err);
+            this.actionMsg(result, err, true);
           }
         );
       }
     });
   }
 
-  update(courseUpdated: ICourseDTO): Observable<ICourseDTO> {
-    const { id, ...course } = courseUpdated;
+  actionMsg(result: any, res: ICourseDTO | any, err=false) {
+    if (err) {
+      this._snackBar.open(`${result.action} Error`, '', { panelClass: ['snackbar--error'] });
+    } else {
+      this._snackBar.open(`${result.action}d ${res.id}`, '', { panelClass: ['snackbar--success'] });
+    }
+  }
+
+  update(courseToUpdate: ICourseDTO): Observable<ICourseDTO> {
+    const { id, ...course } = courseToUpdate;
     return this.coursesService.putCourse(id, course);
   }
 
-  delete(courseUpdated: ICourseDTO): void {}
+  delete(courseToDelete: ICourseDTO): Observable<ICourseDTO> {
+    const { id, ...course } = courseToDelete;
+    return this.coursesService.deleteCourse(id);
+  }
 
-  create(courseUpdated: ICourseDTO): void {}
+  create(courseToCreate: ICourseBase): Observable<ICourseDTO> {
+    return this.coursesService.postCourse(courseToCreate);
+  }
 }
