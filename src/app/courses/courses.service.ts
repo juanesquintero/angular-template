2ff0ed7b0f2ff8c0564f34c13753907cd6b1dfd4
Juan Quintero <juanestquintero@gmail.com>
2022-10-17 00:18:55 -0500

    refactor: courses api endpoint

--- src/app/courses/courses.service.ts
+++ src/app/courses/courses.service.ts
@@ -1,7 +1,6 @@
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { IAppName } from '../shared/models';
 import { environment } from './../../environments/environment';
 import { APP_ROUTES } from '../shared/constants';
 import { ICourseBase, ICourseDTO } from '../shared/models/courses.model';
@@ -9,25 +8,23 @@ import { ICourseBase, ICourseDTO } from '../shared/models/courses.model';
 
 @Injectable()
 export class CoursesService {
-
-  private apiPath: string = environment.apiPath;
-  private endpoint: string = '/' + APP_ROUTES.courses;
+  private endpoint: string = environment.apiPath + '/' + APP_ROUTES.courses;
 
   constructor(private http: HttpClient) { }
 
   getCourses(): Observable<ICourseDTO[]> {
-    return this.http.get<ICourseDTO[]>(this.apiPath + this.endpoint);
+    return this.http.get<ICourseDTO[]>(this.endpoint);
   }
 
   postCourse(courseData: ICourseBase): Observable<ICourseDTO> {
-    return this.http.post<ICourseDTO>(this.apiPath + this.endpoint, courseData);
+    return this.http.post<ICourseDTO>(this.endpoint, courseData);
   }
 
   putCourse(courseId: string, courseData: ICourseBase): Observable<ICourseDTO> {
-    return this.http.put<ICourseDTO>(this.apiPath + this.endpoint + '/' + courseId, courseData);
+    return this.http.put<ICourseDTO>(this.endpoint + '/' + courseId, courseData);
   }
 
   deleteCourse(courseId: string): Observable<ICourseDTO> {
-    return this.http.delete<ICourseDTO>(this.apiPath + this.endpoint + '/' + courseId);
+    return this.http.delete<ICourseDTO>(this.endpoint + '/' + courseId);
   }
 }
