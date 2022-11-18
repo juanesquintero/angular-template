9b5745bb31f912aaa58b4faf1b70143377ef6d97
Juan Quintero <juanestquintero@gmail.com>
2022-11-18 08:53:24 -0500

    test: home service

---
+++ src/app/home/home.service.spec.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { HomeService } from './home.service';
+
+let httpClientSpy: jasmine.SpyObj<HttpClient>;
+httpClientSpy = jasmine.createSpyObj('HttpClient', {
+  get: { api: 'name' }
+});
+
+fdescribe('HomeService', () => {
+  let service: HomeService;
+
+  beforeEach(async () => {
+    service = new HomeService(httpClientSpy)
+  });
+
+  it('should create the service', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it(`should have apiPath`, () => {
+    expect(service['apiPath']).toEqual(environment.apiPath);
+  });
+
+  describe('getAppName()', () => {
+    it('should call homeService getAppName() if init', () => {
+      service.getAppName();
+      expect(httpClientSpy.get).toHaveBeenCalled();
+    });
+  })
+});
