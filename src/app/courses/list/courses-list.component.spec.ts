3c18cf9f82c84d1df518a895303efe157252f4dd
Juan Quintero <juanestquintero@gmail.com>
2022-11-22 00:37:32 -0500

    test: run alla tests

--- src/app/courses/list/courses-list.component.spec.ts
+++ src/app/courses/list/courses-list.component.spec.ts
@@ -1,6 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CoursesListComponent } from './courses-list.component';
+import { CoursesService } from '../courses.service';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { of } from 'rxjs';
+
+const dialog: jasmine.SpyObj<MatDialog> = jasmine.createSpyObj('MatDialog', {
+  open: {
+    afterClosed: () => ({
+      subscribe: (fn: any) => { }
+    })
+  }
+});
+
+const snackbar: jasmine.SpyObj<MatSnackBar> = jasmine.createSpyObj('MatSnackBar', {
+  open: {
+    afterClosed: () => ({
+      subscribe: (fn: any) => { }
+    })
+  }
+});
 
 describe('CoursesListComponent', () => {
   let component: CoursesListComponent;
@@ -8,9 +27,23 @@ describe('CoursesListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CoursesListComponent ]
+      declarations: [CoursesListComponent],
+      providers: [
+        {
+          provide: CoursesService,
+          useValue: { getCourses: () => of([]) },
+        },
+        {
+          provide: MatDialog,
+          useValue: dialog,
+        },
+        {
+          provide: MatSnackBar,
+          useValue: snackbar,
+        },
+      ]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(CoursesListComponent);
     component = fixture.componentInstance;
