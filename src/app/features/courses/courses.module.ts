e15a7d6dc9106f07e74b5241a040405a45bf0672
Juan Quintero <juanestquintero@gmail.com>
2022-12-15 19:06:59 -0500

    chore: compile success for modules

--- src/app/features/courses/courses.module.ts
+++ src/app/features/courses/courses.module.ts
@@ -2,8 +2,8 @@ import { CoursesRoutingModule } from './courses-routing.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CoursesListComponent } from './components/list/courses-list.component';
-import { CoursesService } from './courses.service';
-import { CourseModalComponent } from './modal/course-modal.component';
+import { CoursesService } from './services/courses.service';
+import { CourseModalComponent } from './components/modal/course-modal.component';
 import { MaterialModule } from '@shared/material.module';
 import { AppFormsModule } from '@shared/forms.module';
 
