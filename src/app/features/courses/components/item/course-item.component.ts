0eadbca7af485f77aa1d704d6206100683c26822
Juan Quintero <juanestquintero@gmail.com>
2022-12-28 15:31:25 -0500

    fix: course item columns to list

--- src/app/features/courses/components/item/course-item.component.ts
+++ src/app/features/courses/components/item/course-item.component.ts
@@ -8,11 +8,9 @@ import { Component, OnInit, Input } from '@angular/core';
 export class CourseItemComponent implements OnInit {
   @Input() isAdmin!: boolean;
   @Input() openModal!: any;
-  @Input() tableColumns!: string[];
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
 }
