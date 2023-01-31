1961b0c52d846f216681a0082cb6a49d1b695a98
Juan Quintero <juanestquintero@gmail.com>
2023-01-30 19:04:34 -0500

    style: short lang selector

--- src/app/core/components/lang-selector/lang-selector.component.ts
+++ src/app/core/components/lang-selector/lang-selector.component.ts
@@ -1,4 +1,5 @@
 import { LocaleService } from '@core/services/locale/locale.service';
+import { LG_SIZE_PX, MD_SIZE_PX } from '@shared/utils/constants';
 import { Component, Input } from '@angular/core';
 
 @Component({
@@ -7,5 +8,11 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./lang-selector.component.scss']
 })
 export class LangSelectorComponent {
+  @Input() width!: number;
+
+  public get isLong() : boolean {
+    return this.width > LG_SIZE_PX && this.width < MD_SIZE_PX
+  }
+
   constructor(public locale: LocaleService) { }
 }
